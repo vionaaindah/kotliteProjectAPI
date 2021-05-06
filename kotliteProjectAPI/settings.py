a4f5f8de85c0cfbcd4a48745077e7006eeb49701
Siti Viona Indah Swari <69779014+SVeeIS@users.noreply.github.com>
2021-05-06 17:25:01 +0800

    Update settings.py

--- kotliteProjectAPI/settings.py
+++ kotliteProjectAPI/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'coreapi'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
