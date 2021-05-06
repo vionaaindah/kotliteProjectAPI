a244726aa53c3da443f0c97abad76d855e4124e6
SVeeIS <vionaindah11@gmail.com>
2021-05-06 16:34:26 +0800

    update settings.py and requirement.txt

--- kotliteProjectAPI/settings.py
+++ kotliteProjectAPI/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -82,7 +81,7 @@ WSGI_APPLICATION = 'kotliteProjectAPI.wsgi.application'
 
 # UNCOMMENT THIS CODE FOR SETUP IN GCP
 # import pymysql  # noqa: 402
-# pymysql.version_info = (2, 0, 3, 'final', 0)  # change mysqlclient version
+# pymysql.version_info = (1, 4, 0, 'final', 0)  # change mysqlclient version
 # pymysql.install_as_MySQLdb()
 
 
