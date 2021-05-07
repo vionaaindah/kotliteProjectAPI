c44f19efb655cb3d33027e441b1749dee2be45e0
Siti Viona Indah Swari <69779014+SVeeIS@users.noreply.github.com>
2021-05-07 12:48:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from mysite.wsgi import application
+from kotliteProjectAPI.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
@@ -6,5 +6,5 @@ from mysite.wsgi import application
 # application from mysite/wsgi.py and renames it app so it is discoverable by
 # App Engine without additional configuration.
 # Alternatively, you can add a custom entrypoint field in your app.yaml:
-# entrypoint: gunicorn -b :$PORT mysite.wsgi
+# entrypoint: gunicorn -b :$PORT kotliteProjectAPI.wsgi
 app = application
