589664a25ab9e849d10bb7faf886ffc43ed42251
SVeeIS <vionaindah11@gmail.com>
2021-05-11 13:42:15 +0800

    Update admin site

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Order)
-admin.site.register(FindingDriver)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'lat_start', 'long_start', 'lat_end', 'long_end', )
+    list_filter = ('time', 'status', )
+
+@admin.register(FindingDriver)
+class FindingDriverAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'order', 'lat_start_point', 'long_start_point', 'lat_end_point', 'long_end_point', )
+    list_filter = ('time', )
+    
+admin.site.site_header = "Kotlite Admin"
--- passengers/admin.py
+++ passengers/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Request)
-admin.site.register(Transaction)
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'order', 'lat_pick', 'long_pick', 'lat_drop', 'long_drop', )
+    list_filter = ('time', 'status', )
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'request', 'fee', )
+    list_filter = ('status', )
