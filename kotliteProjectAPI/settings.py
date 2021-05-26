e22899905b44fb654e64822fd06330e210f4f369
SVeeIS <vionaindah11@gmail.com>
2021-05-26 20:32:32 +0800

    Update settings.py

--- kotliteProjectAPI/settings.py
+++ kotliteProjectAPI/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
 }
